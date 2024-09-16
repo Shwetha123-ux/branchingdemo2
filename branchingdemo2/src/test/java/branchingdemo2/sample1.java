145d1ee26edd2a9f8bfa42287b929fa475de259e
Shwetha123-ux <shwetha1113@gmail.com>
2024-09-16 20:47:13 +0530

    comit3

---
+++ branchingdemo2/src/test/java/branchingdemo2/sample1.java
@@ -0,0 +1,10 @@
+package branchingdemo2;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("demobranching ..");
+	}
+
+}
